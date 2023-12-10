eb76096f2fbd49ce0ad8ece99dcf6e7c89c52621
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2023-12-10 15:25:00 +0200

    Add functionality to save preprocessed data into separate CSV files for X_train, X_test, y_train, and y_test. Files are named X_train.csv, X_test.csv, y_train.csv, and y_test.csv. Also, update variable names and comments for clarity

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,16 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 
-def load_and_preprocess_data(file_path, save_path=None):
-    # Load the dataset from the 'data' folder
+def load_and_preprocess_data(file_path, save_path_prefix=''):
     try:
         dataset = pd.read_csv(file_path, encoding='utf-8')
     except UnicodeDecodeError:
-        # If utf-8 fails, try 'latin1' encoding
         dataset = pd.read_csv(file_path, encoding='latin1')
 
     # Drop unnecessary columns for now
@@ -23,7 +20,7 @@ def load_and_preprocess_data(file_path, save_path=None):
     missing_data = dataset.isnull()
     missing_data_count_per_column = missing_data.sum()
 
-    ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])] , remainder='passthrough')
+    ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
     X = np.array(ct.fit_transform(X))
     X = np.delete(X, 3, axis=1)
 
@@ -33,30 +30,18 @@ def load_and_preprocess_data(file_path, save_path=None):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # sc = StandardScaler()
-    # X_train_scaled = X_train
-    # X_test_scaled = X_test
-    # X_train_scaled[:, 3:] = sc.fit_transform(X_train[:, 3:])
-    # X_test_scaled[:, 3:] = sc.transform(X_test[:, 3:])
-
-    
-    column_names = ["Kraków_City", "Warszawa_City", "Poznañ_City", "floor", "rooms", "sq", "year"]
-
     # Convert back to DataFrame for easy visualization (optional)
+    column_names = ["Kraków_City", "Warszawa_City", "Poznañ_City", "floor", "rooms", "sq", "year"]
     X_train_df = pd.DataFrame(X_train, columns=column_names).apply(lambda x: x.map('{:.2f}'.format))
     X_test_df = pd.DataFrame(X_test, columns=column_names).apply(lambda x: x.map('{:.2f}'.format))
 
-
-    # Save the preprocessed data to a new CSV file
-    if save_path:
-        preprocessed_data = pd.concat([X_train_df, X_test_df], ignore_index=True)
-        preprocessed_data.to_csv(save_path, index=False)
-
-    return X_train, X_test, y_train, y_test
-
+    # Save the preprocessed data to separate CSV files
+    X_train_df.to_csv(f'{save_path_prefix}X_train.csv', index=False)
+    X_test_df.to_csv(f'{save_path_prefix}X_test.csv', index=False)
+    pd.DataFrame(y_train, columns=['price']).to_csv(f'{save_path_prefix}y_train.csv', index=False)
+    pd.DataFrame(y_test, columns=['price']).to_csv(f'{save_path_prefix}y_test.csv', index=False)
 
 file_path = 'data/Houses.csv'
+save_path_prefix = 'data/'
 
-save_path = 'data/preprocessed_data.csv'
-
-X_train, X_test, y_train, y_test = load_and_preprocess_data(file_path, save_path)
+load_and_preprocess_data(file_path, save_path_prefix)
